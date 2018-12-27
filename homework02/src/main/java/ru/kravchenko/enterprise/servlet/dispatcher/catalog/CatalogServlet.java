865033210a0f00e050e9b1fb8edf93610b8005b1
roman8361 <torex@inbox.ru>
2018-12-27 21:46:02 +0300

    homework to JavaEE lesson02

---
+++ homework02/src/main/java/ru/kravchenko/enterprise/servlet/dispatcher/catalog/CatalogServlet.java
@@ -0,0 +1,16 @@
+package ru.kravchenko.enterprise.servlet.catalog;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/catalog")
+public class CatalogServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/catalog/catalog.html").forward(req, resp);
+    }
+}
---
+++ homework02/src/main/java/ru/kravchenko/enterprise/servlet/dispatcher/product/ProductServlet.java
@@ -0,0 +1,19 @@
+package ru.kravchenko.enterprise.servlet.product;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/product")
+public class ProductServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("product/product.html").forward(req, resp);
+    }
+
+}
